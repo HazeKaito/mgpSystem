188122b64b15d8c523c8f9a1a9bf023fac610534
HazeKaito <118034421+HazeKaito@users.noreply.github.com>
2026-04-04 01:38:08 +0800

    More widgets added to dashboard, minor login page functionality added

--- obj/Debug/net10.0/mgpSystem.AssemblyInfo.cs
+++ obj/Debug/net10.0/mgpSystem.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("mgpSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6f5b5198c64a416d22108f451ed158a6c982d91")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c7ee4af193fdbe66ee18dc884f1d457840c2ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("mgpSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("mgpSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ wwwroot/models/patient.cs
@@ -0,0 +1,11 @@
+public class Patient
+{
+    public int Id { get; set; } //primary key
+    public string Name { get; set; } //name
+    public int Age { get; set; } //age
+    public string Gender { get; set; } //gender
+    public string Address { get; set; } //address
+    public string Email { get; set; } //email
+    public string PhoneNumber { get; set; } // phone number
+
+}
